856b75ae3c51a3505e933ab7cfb050abfee72df5
Raff Evald <rafael-ev-silva@outlook.com>
2022-06-25 16:24:04 -0300

    Semana Ignite Lab RocketSeat - dia 4

---
+++ src/pages/Subscribe.tsx
@@ -0,0 +1,11 @@
+export function Subscribe() {
+    return (
+        <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
+            
+            <img className="mt-10"
+                src="/src/assets/code-mockup.png"
+                alt=""
+            />
+        </div>
+    )
+}
